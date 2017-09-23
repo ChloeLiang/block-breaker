360428cf7a0277a0a2a2bbc76f06cc934f36bef5
ChloeLiang <nliangxin@gmail.com>
2017-09-23 21:43:11 +0800

    Using static variables
    Bug fix:
    
    - Preventing duplicate music players

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
 
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
