ae96d1be713040480fa0159875a927fec9cf41e1
ChloeLiang <nliangxin@gmail.com>
2017-09-26 15:04:29 +0800

    Detect win condition
    - Add a static breakableCount variable in Brick.cs
    - Access breakableCount in LevelManager

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
+	public static int breakableCount = 0;
 	public Sprite[] hitSprites;
 
 	private int timesHit;
 	private LevelManager levelManager;
+	private bool isBreakable;
 
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		// Keep track of breakable bricks
+		if (isBreakable) {
+			breakableCount++;
+			print(breakableCount);
+		}
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
@@ -20,7 +28,7 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
-		bool isBreakable = (this.tag == "Breakable");
+		
 		if (isBreakable) {
 			HandleHits();
 		}
@@ -30,6 +38,8 @@ public class Brick : MonoBehaviour {
 		timesHit++;
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
+			breakableCount--;
+			levelManager.BrickDestroyed();
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
